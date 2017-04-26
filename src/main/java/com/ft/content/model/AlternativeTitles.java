932c184d9c17b74f84b897316cc3967dbd9af042
radutudor <radu.tudor@ft.com>
2017-04-26 17:44:57 +0300

    added contentPackageTitle to the content model

--- src/main/java/com/ft/content/model/AlternativeTitles.java
+++ src/main/java/com/ft/content/model/AlternativeTitles.java
@@ -6,24 +6,34 @@ import com.google.common.base.MoreObjects;
 import java.util.Objects;
 
 public class AlternativeTitles {
+
   private final String promotionalTitle;
-  
+  private final String contentPackageTitle;
+
   public static Builder builder() {
     return new Builder();
   }
   
-  private AlternativeTitles(@JsonProperty("promotionalTitle") String promotionalTitle) {
+  private AlternativeTitles(
+      @JsonProperty("promotionalTitle") String promotionalTitle,
+      @JsonProperty("contentPackageTitle") String contentPackageTitle) {
     this.promotionalTitle = promotionalTitle;
+    this.contentPackageTitle = contentPackageTitle;
   }
   
   public String getPromotionalTitle() {
     return promotionalTitle;
   }
-  
+
+  public String getContentPackageTitle() {
+    return contentPackageTitle;
+  }
+
   @Override
   public String toString() {
     return MoreObjects.toStringHelper(this)
         .add("promotionalTitle", promotionalTitle)
+        .add("contentPackageTitle", contentPackageTitle)
         .toString();
   }
   
@@ -33,29 +43,42 @@ public class AlternativeTitles {
       return false;
     }
     
-    AlternativeTitles that = (AlternativeTitles)o;
-    return Objects.equals(this.promotionalTitle, that.promotionalTitle);
+    final AlternativeTitles that = (AlternativeTitles)o;
+
+    return
+        Objects.equals(this.promotionalTitle, that.promotionalTitle) &&
+        Objects.equals(this.contentPackageTitle, that.contentPackageTitle);
   }
   
   @Override
   public int hashCode() {
-    return Objects.hash(promotionalTitle);
+    return Objects.hash(promotionalTitle, contentPackageTitle);
   }
   
   public static class Builder {
+
     private String promotionalTitle;
-    
+
+    private String contentPackageTitle;
+
     public Builder withPromotionalTitle(String title) {
         this.promotionalTitle = title;
         return this;
     }
-    
+
+    public Builder withContentPackageTitle(String title) {
+        this.contentPackageTitle = title;
+        return this;
+    }
+
     public Builder withValuesFrom(AlternativeTitles titles) {
-      return withPromotionalTitle(titles.getPromotionalTitle());
+      return
+          withPromotionalTitle(titles.getPromotionalTitle()).
+              withContentPackageTitle(titles.getContentPackageTitle());
     }
 
     public AlternativeTitles build() {
-        return new AlternativeTitles(promotionalTitle);
+        return new AlternativeTitles(promotionalTitle, contentPackageTitle);
     }
   }
 }
