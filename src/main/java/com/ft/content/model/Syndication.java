2ec396d5916a13d195c733f877dacdc92a06c5e4
Ciont, Ioana <Ioana.Ciont@ft.com>
2017-06-16 16:45:36 +0300

    Syndication/Distribution to support case sensitive values

--- src/main/java/com/ft/content/model/Syndication.java
+++ src/main/java/com/ft/content/model/Syndication.java
@@ -1,13 +1,24 @@
 package com.ft.content.model;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.collect.Maps;
 
 public enum Syndication {
     YES("yes"),
     NO("no"),
     VERIFY("verify");
 
-    private String canBeSyndicated;
+	private static Map<String, Syndication> syndicationMap = Maps.newHashMap();
+
+	private String canBeSyndicated;
+    
+	static {
+		for (Syndication s : values()) {
+			syndicationMap.put(s.getCanBeSyndicated(), s);
+		}
+	}
 
     Syndication(String canBeSyndicated) {
         this.canBeSyndicated = canBeSyndicated;
@@ -17,4 +28,8 @@ public enum Syndication {
     public String getCanBeSyndicated() {
         return canBeSyndicated;
     }
+    
+	public static Syndication fromString(String value) {
+		return syndicationMap.get(value.toLowerCase());
+	}
 }
